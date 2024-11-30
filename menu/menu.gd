784212b9948e1255eb84fb8a31648847c5bda363
eliasescalante <eliasw.escalante@gmail.com>
2024-11-30 16:12:13 -0300

    agrego menu, creditos y botones ui

---
+++ menu/menu.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	# Cambia a la escena del nivel usando una ruta
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+
+func _on_creditos_pressed() -> void:
+	# Cambia a la escena de créditos usando una ruta
+	get_tree().change_scene_to_file("res://creditos/creditos.tscn")
+
+func _on_salir_pressed() -> void:
+	# Cierra el juego
+	get_tree().quit()
